460f4c4fd1025322208dd9fcfea95d6e1cd0f5d4
anurag-rathi <anuragrathi66@gmail.com>
2020-09-20 12:13:02 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "322393559179904",
+    clientSecret: "392c2cf219858326e3736dd3b8674146",
+  },
 };
--- routes/users.js
+++ routes/users.js
@@ -81,4 +81,19 @@ router.get("/logout", cors.corsWithOptions, (req, res, next) => {
   }
 });
 
+router.post(
+  "/facebook/token",
+  passport.authenticate("facebook-token", { session: false }),
+  (req, res) => {
+    var token = authenticate.getToken({ _id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json({
+      success: true,
+      token: token,
+      status: "You are successfully logged in!",
+    });
+  }
+);
+
 module.exports = router;
